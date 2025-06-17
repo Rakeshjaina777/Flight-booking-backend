c2efb132e1f22c281e71065f305cbcb3a1776cc1
Merai <intern.rj@merai.co>
2025-06-18 04:42:51 +0530

    Fix(Fix minor bug on  user  dto and controler)

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,4 +1,20 @@
+// update-user.dto.ts
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiPropertyOptional({ example: 'UpdatedName' })
+  name?: string;
+
+  @ApiPropertyOptional({ example: 'updated@example.com' })
+  email?: string;
+
+  @ApiPropertyOptional({ example: 35 })
+  age?: number;
+
+  @ApiPropertyOptional({ example: 'NewPassword@456' })
+  password?: string;
+
+  
+}
