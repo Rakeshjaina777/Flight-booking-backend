d90acebc907cb641f828ecd1914c0e13e2be1149
Merai <intern.rj@merai.co>
2025-06-18 04:13:52 +0530

    feat (prisma module, app module,)

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { FlightModule } from './modules/flight/flight.module';
+import { BookingModule } from './modules/booking/booking.module';
+import { SeatModule } from './modules/seat/seat.module';
+import { FareModule } from './modules/fare/fare.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { RequestLoggerMiddleware } from './common/middleware/request-logger.middleware';
+import { PrismaService } from 'prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+// import { PrismaService } from 'prisma/prisma.service';
+
+@Module({
+  imports: [
+    UserModule,
+    FlightModule,
+    BookingModule,
+    SeatModule,
+    FareModule,
+    AuthModule,
+    PrismaModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+  // exports: [PrismaService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggerMiddleware).forRoutes('*');
+  }
+}
